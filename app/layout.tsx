ef377b9cac46340e3fe4c4ce39fd774fec0f3359
h1xy <48129611+h1xy@users.noreply.github.com>
2024-02-11 22:25:02 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "免费ChatGPT",
+  description: "Free ChatGPT, 免费GPT, 免费AI聊天",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "免费ChatGPT",
     statusBarStyle: "default",
   },
 };
