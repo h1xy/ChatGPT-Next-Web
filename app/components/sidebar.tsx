ef2ffcbc36fc90a7796cdbcbd454f3b992345fda
h1xy <48129611+h1xy@users.noreply.github.com>
2025-11-16 02:43:59 +0800

    Change txt in sidbar
    Updated sidebar text and links to reflect new domain.

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,30 +155,18 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          FreeGPT
+          free.oaibest.com
         </div>
         <div className={styles["sidebar-sub-title"]}>
         <b><a href="https://t.me/AI_News_CN" target="_blank">
-          TG频道
-        </a>
-        &nbsp;&nbsp;&nbsp;
-        <a href="https://pd.qq.com/s/bx1en54s9" target="_blank">
-          QQ频道
-        </a>
-        &nbsp;&nbsp;&nbsp;
-        <a href="https://link3.cc/gpt123" target="_blank">
-          导航页
+          Telegram Channel
         </a>
         &nbsp;&nbsp;&nbsp;
         <a href="https://status.g4f.icu" target="_blank">
-          状态页
-        </a>
-        &nbsp;&nbsp;&nbsp;
-        <a href="/donate.txt" target="_blank">
-          Donate 捐助
+          Status Page
         </a></b>
           <br /><br />
-          1️⃣本站免费使用，关注TG/QQ不失联<br />2️⃣如返回大量HTML代码，请刷新页面<br />3️⃣如回答失败，请重试提问自动轮询其他渠道<br />4️⃣<span style={{ color: 'red' }}><b>现已推出&nbsp;<a href="https://api.g4f.icu" target="_blank">付费版</a>&nbsp;一站式服务支持Claude-3/GPTs/联网/文件上传分析/MJ绘图等</b></span><br />5️⃣Please <a href="/donate.txt" target="_blank">donate</a> if this site have help you to keep our free service<br />6️⃣<a href="https://api.g4f.icu/about" target="_blank">AI API gateway</a> support OpenAI, GPTs, Claude, Midjourney and file upload. Cheap rate and paid by USDT!
+          1️⃣Domain is change to free.oaibest.com please update bookmark<br />2️⃣If answer error, please ask again or switch to other model<br />3️⃣<span style={{ color: 'red' }}><b>Try&nbsp;<a href="https://api.oaibest.com" target="_blank">api.oaibest.com</a>&nbsp;to access 340+ model including OpenAI, Claude, Gemini, Deepseek, Grok, Midjourney and file upload. Cheap rate, support Strip and USDT payment!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -201,9 +189,9 @@ export function SideBar(props: { className?: string }) {
         />
         <IconButton
           icon={<PluginIcon />}
-          text={shouldNarrow ? undefined : "AI API Gateway"}
+          text={shouldNarrow ? undefined : "api.oaibest.com"}
           className={styles["sidebar-bar-button"]}
-          onClick={() => window.open("https://api.g4f.icu/about", "_blank")}
+          onClick={() => window.open("https://api.oaibest.com", "_blank")}
           shadow
         />
       </div>
